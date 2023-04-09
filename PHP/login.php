c45f2ba0e2fd4b0ae391af60e7b72dad52153361
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-04-09 20:26:42 +0530

    new

--- PHP/login.php
+++ PHP/login.php
@@ -1,29 +1,34 @@
 <html>
-<body>
-<form method="post">
-<fieldset>
-Name<input type="text" name="username" required><br><br>
-Password<input type="password" name="password" required><br><br>
-<input type="submit" name="submit">
-</fieldset><br>
-</form>
-</body>
+    <body>
+        <form method="post">
+            <fieldset>
+                <legend>Enter Information</legend>
+                Name <input type="text" name="username" required><br>
+                Password <input type="password" name="password" required><br>
+                <input type="submit" name="submit">
+            </fieldset><br>
+        </form>
+    </body>
 </html>
 <?php
 if(isset($_POST["submit"]))
 {
-	$un=$_POST["username"];
-	$pw=$_POST["password"];
-	$con=pg_connect("host=localhost dbname=loginx port=5432 user=postgres password=amz");
-	echo $con;
-	$r=pg_query($con,"select * from login where username='$un' and password='$pw'");
-	if($n)
-	{
-		echo "login successful"; 
-	}
-	else
-	{
-		echo "please try again";
-	}
+    $un=$_POST["username"];
+    $pw=$_POST["password"];
+    
+    $con=pg_connect("host=127.0.0.1 dbname=loginx port=5432 user=postgres password=amz");
+    echo $con;
+    $r=pg_query($con,"select * from login where username='$un' and password='$pw'");
+    $n=pg_num_rows($r);
+    echo $n;
+    if($n)
+    {
+        echo "successfully logged in";
+    }
+    else
+    {
+        echo "wrong pass";
+    }
 }
+
 ?>
