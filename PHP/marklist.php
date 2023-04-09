af1e4f64b6dca4434f67c1cc4ef2b80d5f3485f5
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-04-09 20:46:22 +0530

    Update marklist.php

--- PHP/marklist.php
+++ PHP/marklist.php
@@ -1,29 +1,30 @@
 <html>
 <body align='center'>
 <form method="POST" action="">
-regno<"input type="text" name="t1">
-<input type="subbmit" value="getresult">
+regno<input type="text" name="t1">
+<input type="submit" value="getresult">
 </form>
 </body>
 </html>
-<?Php
+<?php
 if($_POST){
-$reg=$_POST["t1"];
-echo"<h1 align='center'>MARKLIST</h1>
-$con=pg_connect("host=localhost dbname=student user=postgres password=amz");
-if($_con)
-echo"successfully connected";
-$qry="select * from marklist where rolllno=$reg";
-$marklist=pg_query($con,$qry);
-$nos=pg_num_rows($marklist);
-while($row=pg_fetch_rows($marklist)){
-echo"<br>";
-echo"<table align='center' border='1' cellspacing='0'>
-<tr><th>rollno</th><td>$_row[0]</td></tr>
-<tr><th>name</th><td>$_row[1]</td></tr>
-<tr><th>mark</th><td>$_row[2]</td></tr>
-<tr><th>grade</th><td>$_row[3]</td></tr>
-</table>";
+    $reg = $_POST["t1"];
+    echo "<h1 align='center'>MARKLIST</h1>";
+    $con = pg_connect("host=127.0.0.1 dbname=loginx user=postgres password=amz");
+    if($con) {
+        echo "Successfully connected";
+    }
+    $qry = "SELECT * FROM marklist WHERE rollno=$reg";
+    $marklist = pg_query($con, $qry);
+    $nos = pg_num_rows($marklist);
+    while($row = pg_fetch_row($marklist)){
+        echo "<br>";
+        echo "<table align='center' border='1' cellspacing='0'>
+                <tr><th>rollno</th><td>$row[0]</td></tr>
+                <tr><th>name</th><td>$row[1]</td></tr>
+                <tr><th>mark</th><td>$row[2]</td></tr>
+                <tr><th>grade</th><td>$row[3]</td></tr>
+            </table>";
+    }
 }
-}
-?>`
+?>
